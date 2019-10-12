2d2bc0ca6a2fb1f1a845f34fcd3ccc1f980f1835
unknown <m.santelicesa@gmail.com>
2019-10-12 18:14:41 -0300

    test de maquetas

---
+++ dist/precache-manifest.2050f7b98b51d283a533e1ff36e36404.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "5633e0ceee7549fa3b59",
+    "url": "js/chunk-65b159ae.ffae7d94.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "7213fdd4bcc367863797",
+    "url": "css/chunk-73d426f0.10e1b7ea.css"
+  },
+  {
+    "revision": "406cf6b721529be88517",
+    "url": "css/chunk-1b670d20.ff529071.css"
+  },
+  {
+    "revision": "91b032d7760d0dc997b2",
+    "url": "css/chunk-634b81ed.88246378.css"
+  },
+  {
+    "revision": "91b032d7760d0dc997b2",
+    "url": "js/chunk-634b81ed.63d7f9d1.js"
+  },
+  {
+    "revision": "5633e0ceee7549fa3b59",
+    "url": "css/chunk-65b159ae.5b300ba8.css"
+  },
+  {
+    "revision": "1781f5642aac16ff2db7",
+    "url": "js/app.0a6cc9c5.js"
+  },
+  {
+    "revision": "406cf6b721529be88517",
+    "url": "js/chunk-1b670d20.b1a6c98c.js"
+  },
+  {
+    "revision": "7213fdd4bcc367863797",
+    "url": "js/chunk-73d426f0.caf18bbb.js"
+  },
+  {
+    "revision": "37c9e0c7e42a3a5c995a",
+    "url": "css/chunk-vendors.094d5caf.css"
+  },
+  {
+    "revision": "37c9e0c7e42a3a5c995a",
+    "url": "js/chunk-vendors.64504436.js"
+  },
+  {
+    "revision": "ffc2c5a131ba5c689e590965ca0f3eb6",
+    "url": "index.html"
+  },
+  {
+    "revision": "1781f5642aac16ff2db7",
+    "url": "css/app.30d9fbad.css"
+  }
+];
